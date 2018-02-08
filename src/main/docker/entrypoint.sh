1e4727634dfe1d80f36b483b5bc8604a4ae374fc
Sylvain LAURENT <slaurent@apache.org>
2018-02-08 07:05:52 +0100

    allow both privileged an unprivileged usage

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -2,9 +2,21 @@
 
 set -e
 
-# assuming the container is launched with /intellij-inspect/idea-project-ro bound to the host directory thag contains the IDEA project
-# we create an overlay above /intellij-inspect/idea-project-ro so that we can modify some files in the
+# assuming the container is launched with /home/ijinspector/idea-project-ro bound to the host directory that contains the IDEA project
+# we create an overlay above /home/ijinspector/idea-project-ro so that we can modify some files in the
 # container without affecting the files on the host
-mount -t overlay overlay -o lowerdir=/intellij-inspect/idea-project-ro,upperdir=/intellij-inspect/idea-project-tmprw,workdir=/intellij-inspect/idea-project-overlay-workdir /intellij-inspect/idea-project-tmprw
+echo "Trying to create an overlay /home/ijinspector/idea-project-tmprw above /home/ijinspector/idea-project-ro"
+MOUNT_FAILED=0
+mount -t overlay overlay -o lowerdir=/home/ijinspector/idea-project-ro,upperdir=/home/ijinspector/idea-project-tmprw,workdir=/home/ijinspector/idea-project-overlay-workdir /home/ijinspector/idea-project-tmprw \
+   || MOUNT_FAILED=1
 
-/idea-cli-inspector/ideainspect.groovy -i /idea-IC --resultdir /intellij-inspect/target/inspection-results $*
+if [ $MOUNT_FAILED = 1 ]; then
+    echo "Mount failed, the container is probably not run as --privileged, reverting to symbolic link"
+    cd /home/ijinspector \
+    && rm -rf /home/ijinspector/idea-project-tmprw \
+    && ln -s /home/ijinspector/idea-project-ro /home/ijinspector/idea-project-tmprw
+fi
+
+#set working dir and change user to drop privileges
+cd /home/ijinspector/idea-project-tmprw \
+  && gosu ijinspector /home/ijinspector/groovy/bin/groovy /home/ijinspector/idea-cli-inspector/ideainspect.groovy -i /home/ijinspector/idea-IC --resultdir /home/ijinspector/intellij-inspect/target/inspection-results $*
